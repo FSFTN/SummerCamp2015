a9a7eecc3a0b24e5322aa5f5a802a37e34ff0b26
rajanand02 <rajanand0202@gmail.com>
2015-05-11 17:27:33 +0530

    update contact us form url to .json

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,7 +18,7 @@ $(function() {
         firstName = name.split(' ').slice(0, -1).join(' ');
       }
       $.ajax({
-        url: "https://sc15.herokuapp.com/contacts",
+        url: "https://sc15.herokuapp.com/contacts.json",
         type: "POST",
         dataType: 'json', 
         crossDomain: true,
